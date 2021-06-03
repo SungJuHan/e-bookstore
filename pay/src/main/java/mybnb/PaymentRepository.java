fa324c660f6c7840d123c93cc4f5fdf5b5c40616
SungJuHan <43338817+SungJuHan@users.noreply.github.com>
2021-06-03 14:56:54 +0900

    Delete pay directory

--- pay/src/main/java/mybnb/PaymentRepository.java
+++
@@ -1,8 +0,0 @@
-package mybnb;
-
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface PaymentRepository extends PagingAndSortingRepository<Payment, Long>{
-
-
-}
